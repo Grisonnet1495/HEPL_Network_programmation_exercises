1ce894d63fadcb34bebac261984e495d2fd8ee6d
Grisonnet1495 <146077159+Grisonnet1495@users.noreply.github.com>
2024-11-14 17:39:49 +0100

    Reformatage du Step 2

--- Step2/HandleTCPClient.c
+++ Step2/HandleTCPClient.c
@@ -5,28 +5,27 @@
 #include "LibSer.h"
 #include "Requete.h"
 
-#define RCVBUFSIZE 32   /* Size of receive buffer */
-
+#define RCVBUFSIZE 32 /* Size of receive buffer */
 
 void HandleTCPClient(int clntSocket)
 {
-    int recvMsgSize;                    /* Size of received message */
-    struct Requete UneRequete ;
+  int recvMsgSize; /* Size of received message */
+  struct Requete UneRequete;
 
-    /* Receive message from client */
-    if ((recvMsgSize = recv(clntSocket, &UneRequete, sizeof (struct Requete), 0)) < 0)
-        DieWithError("recv() failed");
-    while(recvMsgSize>0) 
-    {   
-      printf("#%s\n",UneRequete.Chaine) ;       
+  /* Receive message from client */
+  if ((recvMsgSize = recv(clntSocket, &UneRequete, sizeof(struct Requete), 0)) < 0)
+    DieWithError("recv() failed");
+  while (recvMsgSize > 0)
+  {
+    printf("#%s\n", UneRequete.Chaine);
     /* Echo message back to client */
     if (write(clntSocket, &UneRequete, recvMsgSize) != recvMsgSize)
-            DieWithError("send() failed");
-    
+      DieWithError("send() failed");
+
     /* See if there is more data to receive */
-    if ((recvMsgSize = read(clntSocket, &UneRequete, sizeof (struct Requete))) < 0)
-            DieWithError("recv() failed");
-    }
-    printf("Connexion Closed\n")  ;
-    close(clntSocket);    /* Close client socket */
+    if ((recvMsgSize = read(clntSocket, &UneRequete, sizeof(struct Requete))) < 0)
+      DieWithError("recv() failed");
+  }
+  printf("Connexion Closed\n");
+  close(clntSocket); /* Close client socket */
 }
--- Step2/TCPEchoServer.c
+++ Step2/TCPEchoServer.c
@@ -5,10 +5,10 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 
-#define MAXPENDING 5    /* Maximum outstanding connection requests */
+#define MAXPENDING 5 /* Maximum outstanding connection requests */
 
-#include "LibSer.h"  	/* Error handling function */
-#include "HandleTCPClient.h"   /* TCP client handling function */
+#include "LibSer.h"          /* Error handling function */
+#include "HandleTCPClient.h" /* TCP client handling function */
 
 int main(int argc, char *argv[])
 {
@@ -19,20 +19,20 @@ int main(int argc, char *argv[])
     unsigned short echoServPort;     /* Server port */
     unsigned int clntLen;            /* Length of client address data structure */
 
-    if (argc != 2)     /* Test for correct number of arguments */
+    if (argc != 2) /* Test for correct number of arguments */
     {
         fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
         exit(1);
     }
 
-    echoServPort = atoi(argv[1]);  /* First arg:  local port */
+    echoServPort = atoi(argv[1]); /* First arg:  local port */
 
     /* Create socket for incoming connections */
     if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket() failed");
     else
-       printf("socket() Ok\n") ;
-      
+        printf("socket() Ok\n");
+
     /* Construct local address structure */
     memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
     echoServAddr.sin_family = AF_INET;                /* Internet address family */
@@ -40,16 +40,16 @@ int main(int argc, char *argv[])
     echoServAddr.sin_port = htons(echoServPort);      /* Local port */
 
     /* Bind to the local address */
-    if (bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+    if (bind(servSock, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0)
         DieWithError("bind() failed");
     else
-       printf("bind() Ok\n") ;    
+        printf("bind() Ok\n");
 
     /* Mark the socket so it will listen for incoming connections */
     if (listen(servSock, MAXPENDING) < 0)
         DieWithError("listen() failed");
     else
-       printf("listen() Ok\n") ;     
+        printf("listen() Ok\n");
 
     for (;;) /* Run forever */
     {
@@ -57,12 +57,11 @@ int main(int argc, char *argv[])
         clntLen = sizeof(echoClntAddr);
 
         /* Wait for a client to connect */
-        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, 
+        if ((clntSock = accept(servSock, (struct sockaddr *)&echoClntAddr,
                                &clntLen)) < 0)
             DieWithError("accept() failed");
         else
-           printf("accept() Ok\n") ;     
-
+            printf("accept() Ok\n");
 
         /* clntSock is connected to a client! */
 
